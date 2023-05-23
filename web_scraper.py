bd2f640ae548aa5c2a4bcc25123b8e2b70e050e5
adenletchworth <adenrletchworth@gmail.com>
2023-05-22 21:51:41 -0700

    Improved commenting and naming schema

--- web_scraper.py
+++ web_scraper.py
@@ -9,12 +9,20 @@ class MovieScraper:
         self.movie_attributes = []
 
     def scrape_movie_attributes(self):
-        tag_Scrape = requests.get(self.url)
-        tag_html_Content = tag_Scrape.text
-        soup = BeautifulSoup(tag_html_Content, 'lxml')
 
+        # connect to the url
+        movie_Scrape = requests.get(self.url)
+
+        # get content of the page
+        movie_html_Content = movie_Scrape.text
+
+        # connect BeautifulSoup scraper
+        soup = BeautifulSoup(movie_html_Content, 'lxml')
+
+        # get the conent we are interested in
         movie_attributes_r = soup.find_all('span', class_='info-item-value')
 
+        # forms text and adds to movie_attributes list
         for attribute in movie_attributes_r:
             attribute_text = re.sub(r'\s+', ' ', attribute.text.strip())
             self.movie_attributes.append(attribute_text)
