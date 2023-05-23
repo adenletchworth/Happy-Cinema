4d1aef25d7df104550d546024b6e75c2ef11da88
adenletchworth <adenrletchworth@gmail.com>
2023-05-22 22:01:15 -0700

    Improved data structure, added data kept

--- web_scraper.py
+++ web_scraper.py
@@ -6,9 +6,9 @@ import requests
 class MovieScraper:
     def __init__(self, url):
         self.url = url
-        self.movie_attributes = []
+        self.movie_attributes = {}
 
-    def scrape_movie_attributes(self):
+    def get_movie_attributes(self):
 
         # connect to the url
         movie_Scrape = requests.get(self.url)
@@ -21,12 +21,25 @@ class MovieScraper:
 
         # get the conent we are interested in
         movie_attributes_r = soup.find_all('span', class_='info-item-value')
+        movie_labels_r = soup.find_all('b', class_='info-item-label')
 
         # forms text and adds to movie_attributes list
-        for attribute in movie_attributes_r:
+        for label, attribute in zip(movie_labels_r, movie_attributes_r):
+            label_text = label.text.replace(':','').strip()
             attribute_text = re.sub(r'\s+', ' ', attribute.text.strip())
-            self.movie_attributes.append(attribute_text)
+            self.movie_attributes[label_text] = attribute_text
 
     def print_movie_attributes(self):
-        for attribute in self.movie_attributes:
-            print(attribute)
+        for label, attribute in self.movie_attributes.items():
+            print(f"{label}: {attribute}")
+
+
+
+
+url = 'https://www.rottentomatoes.com/m/dark_nature_2022'
+
+scrape = MovieScraper(url)
+
+scrape.get_movie_attributes()
+
+scrape.print_movie_attributes()
