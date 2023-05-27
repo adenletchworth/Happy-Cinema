ef83f13bf812cf84e43fd73d6ab4c266e859d042
adenletchworth <adenrletchworth@gmail.com>
2023-05-27 14:36:58 -0700

    Added genre weighting score implementation

--- language_processing.py
+++ language_processing.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def processInput(user_input: str):
+def processInput(user_description: str, user_genre: str):
     full_movie_data = pd.read_csv('movies_combined.csv')
 
     description_df = full_movie_data['Description']
@@ -16,22 +16,27 @@ def processInput(user_input: str):
 
     lz = WordNetLemmatizer()
 
-    input_tokens = word_tokenize(user_input)
+    input_tokens = word_tokenize(user_description)
     input_words = [lz.lemmatize(word.lower()) for word in input_tokens if word.lower() not in stop_words]
 
     # Encode movie descriptions and user input
+    model = SentenceTransformer('bert-base-nli-mean-tokens')
     encoded_descriptions = model.encode(description_df)
-    encoded_user_input = model.encode([' '.join(input_words)])
+    encoded_user_description = model.encode([' '.join(input_words)])
+
+    encoded_genres = model.encode(genre_df)
+    encoded_user_genre = model.encode([user_genre])
 
     # Calculate cosine similarity
-    similarity_scores = cosine_similarity(encoded_descriptions, encoded_user_input)
+    similarity_description_scores = cosine_similarity(encoded_descriptions, encoded_user_description)
+    similarity_genre_scores = cosine_similarity(encoded_genres, encoded_user_genre)
 
     # Calculate weights
-    description_weight = 0.6
-    genre_weight = 0.4
+    description_weight = .5
+    genre_weight = .5
 
     # Calculate weighted similarity scores
-    weighted_scores = description_weight * similarity_scores
+    weighted_scores = (description_weight * similarity_description_scores) + (genre_weight * similarity_genre_scores)
 
     # Sort movies based on weighted similarity scores
     sorted_indices = weighted_scores.argsort(axis=0)[::-1].flatten()
@@ -46,11 +51,10 @@ def processInput(user_input: str):
 
     return most_similar_movie_title, most_similar_movie_description, most_similar_movie_genre
 
-# Load the pre-trained model for encoding text
-model = SentenceTransformer('bert-base-nli-mean-tokens')
+user_input = 'I want a happy movie with a good ending'
+user_genre = 'Horror'
 
-user_input = 'I want a psychological thriller with a female lead'
-most_similar_movie_title, most_similar_movie_description, most_similar_movie_genre = processInput(user_input)
+most_similar_movie_title, most_similar_movie_description, most_similar_movie_genre = processInput(user_input, user_genre)
 print("Title:", most_similar_movie_title)
 print("Description:", most_similar_movie_description)
 print("Genre:", most_similar_movie_genre)
