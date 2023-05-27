cd8837dea9e89de47c8f7cb82c455c9c14ae8db3
adenletchworth <adenrletchworth@gmail.com>
2023-05-26 22:13:41 -0700

    Worked on frequency of words

--- language_processing.py
+++ language_processing.py
@@ -1,5 +1,5 @@
 from nltk.tokenize import word_tokenize,sent_tokenize
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 import nltk
 import pandas as pd
@@ -13,17 +13,18 @@ stop_words = set(stopwords.words('english'))
 
 words = word_tokenize(description_df[0])
 
-ps = PorterStemmer()
 lz = WordNetLemmatizer()
-
-stemmed_words = [ps.stem(word) for word in words]
-
-filtered_words = [word for word in stemmed_words if not word in stop_words]
-
-lemmatized_words = [lz.lemmatize(word) for word in words]
 
 tagged = nltk.pos_tag(words)
 chunked_words = nltk.ne_chunk(tagged,binary=True)
 
-print(lemmatized_words)
+all_words = []
+
+for sentence in description_df:
+    words = word_tokenize(sentence)
+    for word in words:
+        if word not in stop_words:
+            all_words.append(lz.lemmatize(word.lower()))
  
+print(nltk.FreqDist(all_words).most_common(20))
+
