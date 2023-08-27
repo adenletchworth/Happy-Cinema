6bfa3a87c0e418467269f00ca0e13337b3c64c5c
adenletchworth <adenrletchworth@gmail.com>
2023-08-27 13:28:45 -0700

    Working on control flow

--- main.py
+++ main.py
@@ -7,6 +7,33 @@ from nltk.stem import WordNetLemmatizer
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
+def movieLookup():
+    while True:
+        user_title_input = input('Please Input the Full Title of the Movie: ')
+        guess_title = web_scraper.UrlPredictor(user_title_input)
+        movie_finder =  web_scraper.MovieScraper(guess_title)
+        movie_finder.find_movie_attributes()
+        if movie_finder.movie_attributes == {}:
+            notFoundDec = input('Movie Not Found, Would you like to try again y/n')
+            if notFoundDec == 'y':
+                comparasion()
+            else:
+                continue
+        else:
+            print(movie_finder.movie_attributes)
+            nextDec = input('Is this the movie you are looking for y/n')
+
+            if nextDec == 'y':
+                language_processing.processInput()
+            else:
+                continue
+
+def comparasion():
+    user_description = input("Please Enter a Description to base your Reccomendation off of")
+    user_genre = input("Please Enter a Genre to base your movie reccomendation off of")
+
+    language_processing.processInput(user_description, user_genre)
+
 print('Hello! Welcome to the application. I will give you a movie recommendation!')
 print('We can either try to find a similar movie or you can give us a genre and description')
 
@@ -17,33 +44,7 @@ while not isinstance(user_dec_input, str):
     if not isinstance(user_dec_input, str):
         print('Please enter a valid input')
 
-def movieLookup():
-    while(True):
-        user_title_input = input('Please Input the Full Title of the Movie: ')
-        guess_title = web_scraper.UrlPredictor(user_title_input)
-        movie_finder =  web_scraper.MovieScraper(guess_title)
-        movie_finder.find_movie_attributes()
-        if movie_finder.movie_attributes == {}:
-            notFoundDec = input('Movie Not Found, Would you like to try again y/n')
-            if(notFoundDec == 'y'):
-                continue
-            else:
-                comparasion()
-        else:
-            print(movie_finder.movie_attributes)
-            nextDec = input('Is this the movie you are looking for y/n')
-            
-def comparasion(title='PLACEHOLDER'):
-
-
-
-    
-
-
-def getReccomendation(description):
-    return
-
-
-
-
-
+    if user_dec_input == 'y':
+        movieLookup()
+    else:
+        comparasion()
